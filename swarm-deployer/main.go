0a3410097b352817e7cdcf9de1eb4edff7d5bdcd
sijmenhuizenga <sijmenhuizenga@gmail.com>
2018-07-05 19:04:56 +0200

    Make the swarm deployer smarter

--- swarm-deployer/main.go
+++ swarm-deployer/main.go
@@ -8,47 +8,105 @@ import (
 	"strings"
 )
 
-func main(){
+const currentlyDeployedDir = "/var/tmp/toverspul-currently-deployed"
+
+func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("Usage: toverspul-swarm-deployer [dir]")
 	}
 	repodir := os.Args[1]
 
-	updateStacks(repodir)
-}
-
-func updateStacks(repodir string){
 	log.Println("Going through all directories in " + repodir + " looking for docker-compose.yml files")
-	for _, folder := range findComposeFolders(repodir) {
-		dockerStackDeploy(folder)
+	for _, projectFolder := range findProjectFolders(repodir) {
+		projectName := projectName(projectFolder)
+		log.Println("---- " + projectName + " ----")
+		if isProjectConfigurationChanged(repodir, projectName) {
+			dockerStackRemove(projectName)
+			dockerStackDeploy(projectFolder, projectName)
+		} else if isDockerComposeChanged(repodir, projectName) {
+			dockerStackDeploy(projectFolder, projectName)
+		} else {
+			log.Println("No changes to configuration. Skipping project " + projectName)
+		}
 	}
+
+	log.Println("---- ")
+	copyDir(repodir, currentlyDeployedDir)
 	log.Println("Finished all deployments.")
 }
 
-func findComposeFolders(basedir string) []string {
+func copyDir(src string, target string) {
+	os.RemoveAll(target)
+	cmd := exec.Command("cp", "-R", src, target)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Println("Failed to copy directory from " + src + " to " + target + ":" + err.Error())
+		log.Print(string(out))
+	}
+}
+
+func isProjectConfigurationChanged(repodir string, projectName string) bool {
+	cmd := exec.Command("diff", "-qr", "-x", "docker-compose.yml", currentlyDeployedDir+"/"+projectName, repodir+"/"+projectName)
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		log.Println("Deployment configuration of project " + projectName + " have not changed")
+		return false
+	} else {
+		log.Println("Deployment configuration of project " + projectName + "has changed")
+		log.Print(string(out))
+		return true
+	}
+}
+
+func isDockerComposeChanged(repodir string, projectName string) bool {
+	cmd := exec.Command("diff", "-q", currentlyDeployedDir+"/"+projectName+"/docker-compose.yml", repodir+"/"+projectName+"/docker-compose.yml")
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		log.Println("docker-compose.yml of project " + projectName + " have not changed")
+		return false
+	} else {
+		log.Println("docker-compose.yml of project " + projectName + "has changed")
+		log.Print(string(out))
+		return true
+	}
+}
+
+func findProjectFolders(basedir string) []string {
 	folders, err := filepath.Glob(basedir + "/*/docker-compose.yml")
 	if err != nil {
 		log.Fatal(err)
 	}
 	return Map(folders, func(s string) string {
-		return s[:len(s) - len("/docker-compose.yml")]
+		return s[:len(s)-len("/docker-compose.yml")]
 	})
 }
 
-func dockerStackDeploy(composeFolder string){
-	log.Println("stack deploying in dir " + composeFolder)
-	cmd := exec.Command("docker", "stack", "deploy", "--prune", "--compose-file", "docker-compose.yml", projectName(composeFolder))
-	cmd.Dir = composeFolder
+func dockerStackRemove(projectName string) {
+	log.Println("Removing stack " + projectName)
+	cmd := exec.Command("docker", "stack", "rm", projectName)
 	out, err := cmd.CombinedOutput()
+	log.Print(string(out))
 	if err != nil {
-		log.Println("stack deploy failed " + err.Error() + ": " + string(out))
-		return
+		log.Println("stack removal failed " + err.Error())
+	} else {
+		log.Println("stack deployment success")
 	}
-	log.Println(string(out))
-	log.Println("stack deployment success")
 }
 
-func projectName(composeFolder string) string{
+func dockerStackDeploy(projectFolder string, projectName string) {
+	log.Println("deploying stack in dir " + projectFolder)
+	cmd := exec.Command("docker", "stack", "deploy", "--prune", "--compose-file", "docker-compose.yml", projectName)
+	cmd.Dir = projectFolder
+	out, err := cmd.CombinedOutput()
+	log.Print(string(out))
+	if err != nil {
+		log.Println("stack deploy failed " + err.Error())
+	} else {
+		log.Println("stack deployment success")
+	}
+}
+
+func projectName(composeFolder string) string {
 	split := strings.Split(composeFolder, "/")
 	return split[len(split)-1]
 }
