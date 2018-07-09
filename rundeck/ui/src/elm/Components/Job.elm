3c078df7b176f1f1596ea7c6cccdee5e9ce83696
sijmenhuizenga <sijmenhuizenga@gmail.com>
2018-07-09 13:17:23 +0200

    elm-format

--- rundeck/ui/src/elm/Components/Job.elm
+++ rundeck/ui/src/elm/Components/Job.elm
@@ -1,15 +1,15 @@
 module Components.Job exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (for, placeholder)
-import Html.Events exposing (onClick)
-import Http exposing (Body, expectJson, jsonBody)
-import Bootstrap.Table as Table
-import Bootstrap.Modal as Modal
 import Bootstrap.Button as Button
 import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
 import Bootstrap.Form.Textarea as Textarea
+import Bootstrap.Modal as Modal
+import Bootstrap.Table as Table
+import Html exposing (..)
+import Html.Attributes exposing (for, placeholder)
+import Html.Events exposing (onClick)
+import Http exposing (Body, expectJson, jsonBody)
 import Json.Decode
 import List exposing (filter)
 import List.Extra exposing (replaceIf)
@@ -19,166 +19,229 @@ import Result exposing (Result(Ok))
 import String exposing (isEmpty, join, split)
 
 
-type ModalModus = New | Edit
-type alias Model = {jobs : List Job,
-                    modalVisibility : Modal.Visibility,
-                    modalModus : ModalModus,
-                    modalJob : Job}
+type ModalModus
+    = New
+    | Edit
+
+
+type alias Model =
+    { jobs : List Job
+    , modalVisibility : Modal.Visibility
+    , modalModus : ModalModus
+    , modalJob : Job
+    }
+
 
 emptyJob : Job
-emptyJob = {id = "", title = "", hostnamePattern = "", commands = []}
+emptyJob =
+    { id = "", title = "", hostnamePattern = "", commands = [] }
+
 
 init : Model
-init = {jobs = [],
-        modalVisibility = Modal.hidden,
-        modalModus = Edit,
-        modalJob = emptyJob}
+init =
+    { jobs = []
+    , modalVisibility = Modal.hidden
+    , modalModus = Edit
+    , modalJob = emptyJob
+    }
 
-type Msg = GetJobs
-         | JobsReceived (Result Http.Error (List Job))
-         | JobStored (Result Http.Error Job)
-         | JobDeleted (Result Http.Error Job)
-         | CloseModal
-         | CreateJob
-         | EditJob Job
-         | ModelNewTitle String
-         | ModelNewHostnamePattern String
-         | ModelNewCommands String
-         | ModalSave
-         | ModalDelete
+
+type Msg
+    = GetJobs
+    | JobsReceived (Result Http.Error (List Job))
+    | JobStored (Result Http.Error Job)
+    | JobDeleted (Result Http.Error Job)
+    | CloseModal
+    | CreateJob
+    | EditJob Job
+    | ModelNewTitle String
+    | ModelNewHostnamePattern String
+    | ModelNewCommands String
+    | ModalSave
+    | ModalDelete
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        GetJobs
-            -> (model, getJobsCmd)
-        JobsReceived (Ok jobs)
-            -> ({model | jobs = jobs}, Cmd.none)
-        JobsReceived (Err httpError)
-            -> (model, Cmd.none) --- todo
+        GetJobs ->
+            ( model, getJobsCmd )
 
-        JobStored (Ok job)
-            -> (model |> (if model.modalModus == New then createJob else updateSingleJob) job |> update CloseModal |> Tuple.first, Cmd.none ) --- todo for real
-        JobStored (Err httpError)
-            -> (model, Cmd.none) --- todo
+        JobsReceived (Ok jobs) ->
+            ( { model | jobs = jobs }, Cmd.none )
 
-        JobDeleted (Ok job)
-            -> (model |> deleteJob job |> update CloseModal |> Tuple.first, Cmd.none ) --- todo for real
-        JobDeleted (Err httpError)
-            -> (model, Cmd.none) --- todo
+        JobsReceived (Err httpError) ->
+            ( model, Cmd.none )
 
-        CreateJob
-            -> ( { model | modalVisibility = Modal.shown, modalJob = emptyJob, modalModus = New} , Cmd.none )
-        EditJob job
-            -> ( { model | modalVisibility = Modal.shown, modalJob = job, modalModus = Edit } , Cmd.none )
+        --- todo
+        JobStored (Ok job) ->
+            ( model
+                |> (if model.modalModus == New then
+                        createJob
+                    else
+                        updateSingleJob
+                   )
+                    job
+                |> update CloseModal
+                |> Tuple.first
+            , Cmd.none
+            )
 
-        CloseModal
-            -> ( { model | modalVisibility = Modal.hidden } , Cmd.none )
-        ModalSave
-            -> (model, if model.modalModus == New then createJobCmd model.modalJob else storeJobCmd model.modalJob)
-        ModalDelete
-            -> (model, deleteJobCmd model.modalJob)
+        --- todo for real
+        JobStored (Err httpError) ->
+            ( model, Cmd.none )
 
-        ModelNewTitle newTitle
-            -> ( newTitle
-                    |> asJobTitleIn model.modalJob
-                    |> asModalJobIn model
-                , Cmd.none)
-        ModelNewHostnamePattern newPattern
-            -> ( newPattern
-                    |> asJobPatternIn model.modalJob
-                    |> asModalJobIn model
-                , Cmd.none)
-        ModelNewCommands newCommands
-            -> ( newCommands
-                    |> split "\n"
-                    |> asJobCommands model.modalJob
-                    |> asModalJobIn model
-                , Cmd.none)
+        --- todo
+        JobDeleted (Ok job) ->
+            ( model |> deleteJob job |> update CloseModal |> Tuple.first, Cmd.none )
+
+        --- todo for real
+        JobDeleted (Err httpError) ->
+            ( model, Cmd.none )
+
+        --- todo
+        CreateJob ->
+            ( { model | modalVisibility = Modal.shown, modalJob = emptyJob, modalModus = New }, Cmd.none )
+
+        EditJob job ->
+            ( { model | modalVisibility = Modal.shown, modalJob = job, modalModus = Edit }, Cmd.none )
+
+        CloseModal ->
+            ( { model | modalVisibility = Modal.hidden }, Cmd.none )
+
+        ModalSave ->
+            ( model
+            , if model.modalModus == New then
+                createJobCmd model.modalJob
+              else
+                storeJobCmd model.modalJob
+            )
+
+        ModalDelete ->
+            ( model, deleteJobCmd model.modalJob )
+
+        ModelNewTitle newTitle ->
+            ( newTitle
+                |> asJobTitleIn model.modalJob
+                |> asModalJobIn model
+            , Cmd.none
+            )
+
+        ModelNewHostnamePattern newPattern ->
+            ( newPattern
+                |> asJobPatternIn model.modalJob
+                |> asModalJobIn model
+            , Cmd.none
+            )
+
+        ModelNewCommands newCommands ->
+            ( newCommands
+                |> split "\n"
+                |> asJobCommands model.modalJob
+                |> asModalJobIn model
+            , Cmd.none
+            )
+
 
 setModalJob : Job -> Model -> Model
 setModalJob newJob model =
-    {model | modalJob = newJob}
+    { model | modalJob = newJob }
+
 
 asModalJobIn : Model -> Job -> Model
-asModalJobIn = flip setModalJob
+asModalJobIn =
+    flip setModalJob
+
 
 asJobWithoutEmptyCommands : Job -> Job
 asJobWithoutEmptyCommands job =
-    {job | commands = filter (not << String.isEmpty) job.commands}
+    { job | commands = filter (not << String.isEmpty) job.commands }
 
 
 updateSingleJob : Job -> Model -> Model
 updateSingleJob job model =
-    {model | jobs = model.jobs |> replaceIf (\j -> j.id == job.id) job }
+    { model | jobs = model.jobs |> replaceIf (\j -> j.id == job.id) job }
+
 
 createJob : Job -> Model -> Model
 createJob job model =
-    {model | jobs = job :: model.jobs }
+    { model | jobs = job :: model.jobs }
+
 
 deleteJob : Job -> Model -> Model
 deleteJob job model =
-    {model | jobs = model.jobs |> filter (\j -> j.id /= job.id) }
+    { model | jobs = model.jobs |> filter (\j -> j.id /= job.id) }
+
 
 viewJobsTable : Model -> Html Msg
 viewJobsTable model =
-    div [] [
-        Table.table {
-                options = [ Table.striped, Table.hover ],
-                thead = Table.simpleThead [
-                  Table.th [] [ text "Id"]
-                  , Table.th [] [ text "Title"]
-                  , Table.th [] [ text "Hostname Pattern"]
-                  , Table.th [] [ text "Commands"]
-                  , Table.th [] [ Button.button [ Button.outlineSuccess, Button.small, Button.attrs [ onClick <| CreateJob ] ] [text "New Job"] ]
-                ],
-                tbody = Table.tbody [] (List.map viewJobRow model.jobs)
+    div []
+        [ Table.table
+            { options = [ Table.striped, Table.hover ]
+            , thead =
+                Table.simpleThead
+                    [ Table.th [] [ text "Id" ]
+                    , Table.th [] [ text "Title" ]
+                    , Table.th [] [ text "Hostname Pattern" ]
+                    , Table.th [] [ text "Commands" ]
+                    , Table.th [] [ Button.button [ Button.outlineSuccess, Button.small, Button.attrs [ onClick <| CreateJob ] ] [ text "New Job" ] ]
+                    ]
+            , tbody = Table.tbody [] (List.map viewJobRow model.jobs)
             }
-    ]
+        ]
 
 
 viewJobRow : Job -> Table.Row Msg
 viewJobRow job =
-    Table.tr [] [
-        Table.td [] [ text job.id]
-        , Table.td [] [ text job.title]
-        , Table.td [] [ text job.hostnamePattern]
-        , Table.td [] [ text (join ", " job.commands)]
-        , Table.td [] [
-            Button.button [ Button.info, Button.attrs [ onClick (EditJob job) ]][ text "Edit" ]
+    Table.tr []
+        [ Table.td [] [ text job.id ]
+        , Table.td [] [ text job.title ]
+        , Table.td [] [ text job.hostnamePattern ]
+        , Table.td [] [ text (join ", " job.commands) ]
+        , Table.td []
+            [ Button.button [ Button.info, Button.attrs [ onClick (EditJob job) ] ] [ text "Edit" ]
+            ]
         ]
-    ]
+
 
 viewModal : Model -> Html Msg
 viewModal model =
-    div [] [
-      Modal.config CloseModal
-        |> Modal.large
-        |> Modal.hideOnBackdropClick True
-        |> Modal.h3 [] [ text (if model.modalModus == New then "New Job" else "Edit " ++ model.modalJob.title) ]
-        |> Modal.body [] [viewEditForm model.modalJob]
-        |> Modal.footer [] [
-            Button.button [ Button.outlineDanger, Button.attrs [ onClick ModalDelete ]][ text "Delete" ],
-            Button.button [ Button.outlineWarning, Button.attrs [ onClick CloseModal ]][ text "Close without saving" ],
-            Button.button [ Button.outlineSuccess, Button.attrs [ onClick ModalSave ]][ text "Save" ]]
-        |> Modal.view model.modalVisibility
+    div []
+        [ Modal.config CloseModal
+            |> Modal.large
+            |> Modal.hideOnBackdropClick True
+            |> Modal.h3 []
+                [ text
+                    (if model.modalModus == New then
+                        "New Job"
+                     else
+                        "Edit " ++ model.modalJob.title
+                    )
+                ]
+            |> Modal.body [] [ viewEditForm model.modalJob ]
+            |> Modal.footer []
+                [ Button.button [ Button.outlineDanger, Button.attrs [ onClick ModalDelete ] ] [ text "Delete" ]
+                , Button.button [ Button.outlineWarning, Button.attrs [ onClick CloseModal ] ] [ text "Close without saving" ]
+                , Button.button [ Button.outlineSuccess, Button.attrs [ onClick ModalSave ] ] [ text "Save" ]
+                ]
+            |> Modal.view model.modalVisibility
         ]
+
 
 viewEditForm : Job -> Html Msg
 viewEditForm job =
-    div [] [
-        Form.form []
+    div []
+        [ Form.form []
             [ Form.group []
                 [ Form.label [] [ text "Title" ]
-                , Input.text [Input.value job.title, Input.onInput ModelNewTitle]
+                , Input.text [ Input.value job.title, Input.onInput ModelNewTitle ]
                 ]
             , Form.group []
                 [ Form.label [] [ text "Hostname Pattern" ]
-                , Input.text [Input.value job.hostnamePattern, Input.onInput ModelNewHostnamePattern]
+                , Input.text [ Input.value job.hostnamePattern, Input.onInput ModelNewHostnamePattern ]
                 ]
             , Form.group []
-                [ label [ for "commandsarea"] [ text "Commands"]
+                [ label [ for "commandsarea" ] [ text "Commands" ]
                 , Textarea.textarea
                     [ Textarea.id "commandsarea"
                     , Textarea.rows 3
@@ -186,38 +249,55 @@ viewEditForm job =
                     , Textarea.onInput ModelNewCommands
                     ]
                 ]
-            ]]
+            ]
+        ]
 
 
 getJobsCmd : Cmd Msg
-getJobsCmd = Http.send JobsReceived (Http.get "http://localhost:8090/jobs" (Json.Decode.list jobDecoder))
+getJobsCmd =
+    Http.send JobsReceived (Http.get "http://localhost:8090/jobs" (Json.Decode.list jobDecoder))
+
 
 storeJobCmd : Job -> Cmd Msg
-storeJobCmd job = Http.send JobStored (Http.request {
-                    method = "PUT"
-                    , url = "http://localhost:8090/jobs/" ++ job.id
-                    , body = job |> asJobWithoutEmptyCommands |> jobEncoder |> jsonBody
-                    , headers = []
-                    , timeout = Nothing
-                    , expect = expectJson jobDecoder
-                    , withCredentials = False } )
+storeJobCmd job =
+    Http.send JobStored
+        (Http.request
+            { method = "PUT"
+            , url = "http://localhost:8090/jobs/" ++ job.id
+            , body = job |> asJobWithoutEmptyCommands |> jobEncoder |> jsonBody
+            , headers = []
+            , timeout = Nothing
+            , expect = expectJson jobDecoder
+            , withCredentials = False
+            }
+        )
+
 
 deleteJobCmd : Job -> Cmd Msg
-deleteJobCmd job = Http.send JobDeleted (Http.request {
-                    method = "DELETE"
-                    , url = "http://localhost:8090/jobs/" ++ job.id
-                    , body = Http.emptyBody
-                    , headers = []
-                    , timeout = Nothing
-                    , expect = expectJson jobDecoder
-                    , withCredentials = False } )
+deleteJobCmd job =
+    Http.send JobDeleted
+        (Http.request
+            { method = "DELETE"
+            , url = "http://localhost:8090/jobs/" ++ job.id
+            , body = Http.emptyBody
+            , headers = []
+            , timeout = Nothing
+            , expect = expectJson jobDecoder
+            , withCredentials = False
+            }
+        )
+
 
 createJobCmd : Job -> Cmd Msg
-createJobCmd job = Http.send JobStored (Http.request {
-                   method = "POST"
-                   , url = "http://localhost:8090/jobs"
-                   , body = job |> asJobWithoutEmptyCommands |> jobEncoder |> jsonBody
-                   , headers = []
-                   , timeout = Nothing
-                   , expect = expectJson jobDecoder
-                   , withCredentials = False } )
+createJobCmd job =
+    Http.send JobStored
+        (Http.request
+            { method = "POST"
+            , url = "http://localhost:8090/jobs"
+            , body = job |> asJobWithoutEmptyCommands |> jobEncoder |> jsonBody
+            , headers = []
+            , timeout = Nothing
+            , expect = expectJson jobDecoder
+            , withCredentials = False
+            }
+        )
--- rundeck/ui/src/elm/Main.elm
+++ rundeck/ui/src/elm/Main.elm
@@ -1,52 +1,86 @@
 module Main exposing (..)
-import Html exposing (..)
+
 import Bootstrap.Grid as Grid
 import Components.Job exposing (viewJobsTable)
+import Html exposing (..)
+
 
 -- APP
+
+
 init : Model
-init = {jobsmodel = Components.Job.init, error = ""}
+init =
+    { jobsmodel = Components.Job.init, error = "" }
+
 
 main : Program Never Model Msg
 main =
-  program { init = (init, refreshAllCmd),
-            view = view,
-            update = update,
-            subscriptions = \_ -> Sub.none }
+    program
+        { init = ( init, refreshAllCmd )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
 
 refreshAllCmd : Cmd Msg
-refreshAllCmd = Cmd.batch [
-        Cmd.map JobMsg Components.Job.getJobsCmd
-    ]
+refreshAllCmd =
+    Cmd.batch
+        [ Cmd.map JobMsg Components.Job.getJobsCmd
+        ]
 
--- MODEL`
-type alias JobsModel = Components.Job.Model
-type alias Model = {
-    jobsmodel : JobsModel,
-    error : String}
+
+
+-- MODEL
+
+
+type alias JobsModel =
+    Components.Job.Model
+
+
+type alias Model =
+    { jobsmodel : JobsModel
+    , error : String
+    }
+
 
 
 -- UPDATE
-type Msg = NoOp | JobMsg Components.Job.Msg
+
+
+type Msg
+    = NoOp
+    | JobMsg Components.Job.Msg
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    JobMsg msg_ ->  let (jobsmodel_, cmd_) = Components.Job.update msg_ model.jobsmodel
-                    in ({model | jobsmodel = jobsmodel_}, Cmd.map JobMsg cmd_)
-    NoOp -> (model, Cmd.none)
+    case msg of
+        JobMsg msg_ ->
+            let
+                ( jobsmodel_, cmd_ ) =
+                    Components.Job.update msg_ model.jobsmodel
+            in
+            ( { model | jobsmodel = jobsmodel_ }, Cmd.map JobMsg cmd_ )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-    div [] [
-        Html.map JobMsg (Components.Job.viewModal model.jobsmodel),
-        Grid.container [] [
-            Grid.row [] [
-              Grid.col [] [
-                p [] [ text (model.error)],
-                Html.map JobMsg (viewJobsTable model.jobsmodel)
-              ]
+    div []
+        [ Html.map JobMsg (Components.Job.viewModal model.jobsmodel)
+        , Grid.container []
+            [ Grid.row []
+                [ Grid.col []
+                    [ p [] [ text model.error ]
+                    , Html.map JobMsg (viewJobsTable model.jobsmodel)
+                    ]
+                ]
             ]
-          ]
-    ]
+        ]
