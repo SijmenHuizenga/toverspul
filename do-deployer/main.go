9e770372a5eba7305a5010ff8a0cf227ad661d91
sijmenhuizenga <sijmenhuizenga@gmail.com>
2018-07-13 17:38:15 +0200

    Add privat ekey deployment

--- do-deployer/main.go
+++ do-deployer/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"golang.org/x/oauth2"
-	"github.com/digitalocean/godo"
-	"context"
 	"log"
 	"os"
 	"github.com/BurntSushi/toml"
+	"github.com/digitalocean/godo"
+	"context"
+	"io/ioutil"
+	"os/exec"
 )
 
 func main() {
@@ -19,22 +21,32 @@ chmod +x /usr/local/bin/toverspul-server-installer
 
 wget https://raw.githubusercontent.com/SijmenHuizenga/toverspul/master/git-cloner/toverspul-git-cloner -O /usr/local/bin/toverspul-git-cloner
 chmod +x /usr/local/bin/toverspul-git-cloner
-crontab -l | { cat; echo "*/14 * * * * /usr/local/bin/toverspul-git-cloner /toverspul-config https://github.com/SijmenHuizenga/toverspul-config.git >> /var/log/toverspul-git-cloner.log 2>&1"; } | crontab -
 `
-
-	key := godo.DropletCreateSSHKey{Fingerprint: config.DropletSShFingerprint}
 	tokenSource := &TokenSource{AccessToken: config.DoToken}
-	image := godo.DropletCreateImage{Slug: config.DropletImage}
-
 	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
 	client := godo.NewClient(oauthClient)
+	ctx := context.TODO()
+
+	serverPubKey := makeKeypair(config.DropletName)
+	serverPubKeyRequest := godo.KeyCreateRequest{
+		Name: "toverspul-rundeck-" + config.DropletName,
+		PublicKey: serverPubKey,
+	}
+	serverKey, _, err := client.Keys.Create(ctx, &serverPubKeyRequest)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	createRequest := &godo.DropletCreateRequest{
 		Name:              config.DropletName,
 		Region:            config.DropletRegion,
 		Size:              config.DropletSize,
-		SSHKeys:           []godo.DropletCreateSSHKey{key},
-		Image:             image,
+		SSHKeys:           []godo.DropletCreateSSHKey{
+			{Fingerprint: config.DropletSShFingerprint},
+			{Fingerprint: serverKey.Fingerprint},
+		},
+		Image:             godo.DropletCreateImage{Slug: config.DropletImage},
 		PrivateNetworking: true,
 		Monitoring:        true,
 		Backups:           false,
@@ -42,12 +54,14 @@ crontab -l | { cat; echo "*/14 * * * * /usr/local/bin/toverspul-git-cloner /tove
 		Tags:              []string{"toverspul", config.DropletTag},
 	}
 
-	ctx := context.TODO()
-	_, _, err := client.Droplets.Create(ctx, createRequest)
+
+	droplet, _, err := client.Droplets.Create(ctx, createRequest)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Droplet " + droplet.Name + " created.")
 }
 
 type TokenSource struct {
@@ -71,6 +85,28 @@ type Config struct {
 	SwarmIp               string
 	SwarmToken            string
 	DoToken               string
+}
+
+func makeKeypair(hostname string) string {
+	keyfile := os.Getenv("HOME") + "/.ssh/toverspul/rundeck-" + hostname
+
+	if _, err := os.Stat(keyfile + ".pub"); err == nil {
+		log.Println("keyfile " + keyfile + " already exist. Using existing public key")
+	} else {
+		cm := exec.Command("ssh-keygen", "-t", "rsa", "-b", "4096", "-C", "rundeck@"+hostname, "-N", "", "-f", keyfile)
+		out, err := cm.CombinedOutput()
+		if err != nil {
+			log.Println(string(out))
+			log.Fatal("ssh-keygen failed: ", err)
+		}
+		log.Println("Private key for this server generated and be found in " + keyfile)
+	}
+
+	pubKey, err := ioutil.ReadFile(keyfile + ".pub")
+	if err != nil {
+		log.Fatal("pub key could not be read ", err)
+	}
+	return string(pubKey)
 }
 
 func ReadConfig() Config {
